3de7009493cbf70d03be0a8727214e1d2eb854ce
clacck <45016567+clacck@users.noreply.github.com>
2019-07-31 01:48:43 +0700

    Update front-end

---
+++ search_engine/Library.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <conio.h>
+using namespace std;
+
+
+void Input(string &s);
+void PrStr(const char* name, int x, int y, int color);
+void OutputFunction(const char* name, int x, int y, bool chosen);
+void OutputString(string name, int x, int y, bool chosen);
+void ClearFrame(int x, int y, int h, int w);
+void CPointer(int x, int y);
+void ChangeText(string &s);
+void OutText(const char* name, int x, int y, const char* size);
+void OutPicture(const char* name, int x, int y);
+void ChangeTextColor(int color);
+void PaintFrame(int x, int y, int h, int w);
+bool InputChar(string &res, int x, int y, bool password, int bound, bool edit); 
--- search_engine/main.cpp
+++ search_engine/main.cpp
@@ -4,18 +4,22 @@
  *  Created on: Jul 26, 2019
  *      Author: huy
  */
-#include"System.h"
+#include "System.h"
+#include "Library.h"
+
 int main(){
 	System SYSTEM;
 	//SYSTEM.get_files_name();
-	SYSTEM.InputFiles();
-	SYSTEM.load_stop_word();
-	SYSTEM.build_trie();
-	SYSTEM.inputSearch();
-	SYSTEM.process_type(SYSTEM.getSearch());
+	string s;
+	Input(s);
+	//SYSTEM.InputFiles();
+//	SYSTEM.load_stop_word();
+	//SYSTEM.build_trie();
+	//SYSTEM.inputSearch();
+//	SYSTEM.process_type(SYSTEM.getSearch());
 	//SYSTEM.process_intitle(SYSTEM.getSearch());
 	//SYSTEM.process_a_price();
-	system("pause");
+	system("pause>nul");
 	return 0;
 }
 
